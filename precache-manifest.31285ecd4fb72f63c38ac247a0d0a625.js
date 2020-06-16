ee2c41f4a345a0d5c1f09699389e6d165cf0eec0
aeroman256 <zakharov.dmitry.92@yandex.ru>
2020-06-16 10:56:12 +0300

    prod

---
+++ precache-manifest.31285ecd4fb72f63c38ac247a0d0a625.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de25dd88f1d65e0034d64bf06d249bea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca8c85878916a6bfb94e",
+    "url": "/static/css/main.70c78c6d.chunk.css"
+  },
+  {
+    "revision": "23e6e7645b196a6f3c6a",
+    "url": "/static/js/2.9f835767.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9f835767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8c85878916a6bfb94e",
+    "url": "/static/js/main.bb425a82.chunk.js"
+  },
+  {
+    "revision": "452efe56c2036ff11208",
+    "url": "/static/js/runtime-main.83c66be5.js"
+  }
+]);
